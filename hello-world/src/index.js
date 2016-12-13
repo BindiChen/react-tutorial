1be75154732bdbebb98ca2e9dedcfe8be9f23cbc
BindiChen <bindi.chen@hotmail.com>
2016-12-13 10:35:27 +0000

    Lists and keys

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -268,7 +268,7 @@ const messages = ['React', 'Re: React', 'Re:Re: React'];
     );
  }
 
- class Page extends React.Component {
+ class ul extends React.Component {
     constructor(props) {
         super(props);
         this.state = {showWarning: true};
@@ -293,10 +293,75 @@ const messages = ['React', 'Re: React', 'Re:Re: React'];
     }
  }
 
+ /**
+  * Lists and Keys
+  **/
+const numbers = [1,2,3,4,5];
+// const doubled = numbers.map((number) => number * 2);
+// console.log(doubled);
+
+// const listItems = numbers.map((number) => 
+//     <li>{number}</li>
+// );
+
+
+function NumberList(props) {
+    const numbers = props.numbers;
+
+
+    // const listItems = numbers.map((number) =>
+    //     <li key={number.toString()}>{number}</li>
+    // );
+
+    // console.log(listItems);
+
+    // return (
+    //     <ul>{listItems}</ul>
+    // );
+
+    /* Embedding map() in JSX */
+    return (
+        <ul>
+            {numbers.map((number) => 
+                <li key={number.toString()}>{number}</li>
+            )}
+        </ul>
+    );
+}
+
+function Blog(props) {
+    const sidebar = (
+        <ul>{props.posts.map((post) =>
+                <li key={post.id}>{post.title}</li>
+            )}
+        </ul>
+    );
+
+    const content = props.posts.map((post) => 
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+        </div>
+    );
+
+    return (
+        <div>
+            {sidebar}
+            <hr/>
+            {content}
+        </div>
+    );
+}
+
+const posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm.'}    
+];
+
 
 function App() {
     return (
-        <Page></Page>
+        <NumberList numbers={numbers}></NumberList>
     );
 }
 
